33ac5939329de8ccdb1659ce2da4110e3d88c1be
Kyle Tran <trankyle2002@yahoo.com>
2025-04-17 10:06:44 -0700

    fixes for login, todo: standardizing working parts to rest of code

--- docker-entrypoint-initdb.d/06_user_functions.sql
+++ docker-entrypoint-initdb.d/06_user_functions.sql
@@ -9,7 +9,7 @@ CREATE OR REPLACE FUNCTION create_user (
     DOB_param BIGINT,
     organization_name_param TEXT
 )
-RETURNS user_id AS $$
+RETURNS BOOLEAN AS $$
 DECLARE
     user_id BIGINT;
     org_id BIGINT;
@@ -27,7 +27,7 @@ BEGIN
 
     IF user_id = -1 THEN
         RAISE EXCEPTION 'User Unique Violation';
-        RETURN -1;
+        RETURN FALSE;
     END IF;
     
     IF organization_name_param IS NOT NULL THEN 
@@ -43,7 +43,7 @@ BEGIN
 
             IF result IS NULL THEN
                 RAISE EXCEPTION 'Organization User Mapping not created: Invalid Input';
-                RETURN -1;
+                RETURN FALSE;
             END IF;
         END IF;
     
@@ -57,10 +57,10 @@ BEGIN
 
     IF result IS NULL THEN
         RAISE EXCEPTION 'Organization User Mapping default not created';
-        RETURN -1;
+        RETURN FALSE;
     END IF;
     
-    RETURN user_id;
+    RETURN TRUE;
 END;
 $$ LANGUAGE plpgsql;
 
